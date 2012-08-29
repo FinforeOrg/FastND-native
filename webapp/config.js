19da55d0e07e9144f5d2096deda9b997b5f28a9a
Cristian Colceriu <contact@ghinda.net>
2012-08-29 15:51:38 +0300

    Restructure folders in native repo, to be able to adapt it to a automatic native-build system later on.

---
+++ webapp/config.js
@@ -0,0 +1,15 @@
+/*
+ * FastND Configs
+ * 
+ * Loaded separately, not compressed.
+ */
+
+var finforeBaseUrl = 'http://fastnd.com', // web service url
+	finforeAppUrl = 'http://fastnd.com/', // web app url
+	finforeNative = true,
+	finforeNativeUrl = window.location.href; // web app url
+	
+finforeNativeUrl = finforeNativeUrl.replace(finforeNativeUrl.replace(/^.*[\\\/]/, ''), ''); // remove file.html from path
+
+// Make sure there's a trailing slash
+if(finforeNativeUrl.charAt(finforeNativeUrl.length - 1) != '/') finforeNativeUrl += '/';
